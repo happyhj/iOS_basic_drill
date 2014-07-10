12d2696e4472fb3ac5c3835d9d99e694ff55fa96
KIM HEE JAE <kimheejae@KIMs-MacBook-Pro.local>
2014-07-10 15:16:06 +0900

    Initial Commit

---
+++ week1/FileString/FileString/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FileString
+//
+//  Created by KIM HEE JAE on 7/10/14.
+//  Copyright (c) 2014 ___NHNNEXT___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
