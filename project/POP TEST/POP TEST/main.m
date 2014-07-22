ea3383a9a5ee362ae5f26f08d4da168491b42945
KIM HEE JAE <kimheejae@KIMs-MacBook-Pro.local>
2014-07-22 16:05:12 +0900

    Initial Commit

---
+++ project/POP TEST/POP TEST/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  POP TEST
+//
+//  Created by KIM HEE JAE on 7/22/14.
+//  Copyright (c) 2014 ___NHNNEXT___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
