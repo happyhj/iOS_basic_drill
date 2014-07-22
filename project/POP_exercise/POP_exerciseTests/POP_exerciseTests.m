ed320cf05a28eca70b8b8840c8e60bb24c25cae8
KIM HEE JAE <kimheejae@KIMs-MacBook-Pro.local>
2014-07-22 16:15:50 +0900

    Initial Commit

---
+++ project/POP_exercise/POP_exerciseTests/POP_exerciseTests.m
@@ -0,0 +1,34 @@
+//
+//  POP_exerciseTests.m
+//  POP_exerciseTests
+//
+//  Created by KIM HEE JAE on 7/22/14.
+//  Copyright (c) 2014 ___NHNNEXT___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface POP_exerciseTests : XCTestCase
+
+@end
+
+@implementation POP_exerciseTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
