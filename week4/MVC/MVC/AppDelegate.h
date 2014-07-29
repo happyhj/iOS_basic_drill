c513f86baeae0bcc8047ec036e076ecc5e87629a
happyhj <happyhj@gmail.com>
2014-07-29 14:36:35 +0900

    Initial Commit

---
+++ week4/MVC/MVC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MVC
+//
+//  Created by KIM HEE JAE on 7/29/14.
+//  Copyright (c) 2014 ___NHNNEXT___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
