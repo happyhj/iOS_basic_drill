c8dd65644500bced5d9b400dd13123d3597327f1
happyhj <happyhj@gmail.com>
2014-07-31 15:07:33 +0900

    Initial Commit

--- project/basic_POP/basic_POP/ViewController.h
+++ project/basic_POP/basic_POP/ViewController.h
@@ -9,5 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    BOOL _isInFullscreen;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+- (void)performFullScreenAnimation;
 
 @end
--- project/basic_POP/basic_POP/ViewController.m
+++ project/basic_POP/basic_POP/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <POP/POP.h>
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *widthConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *heightConstraint;
+
 
 @end
 
@@ -18,12 +22,54 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _imageView.userInteractionEnabled = YES;
+    UIPinchGestureRecognizer *pgr = [[UITapGestureRecognizer alloc]
+                                     initWithTarget:self action:@selector(tapGesturePerformed:)];
+    pgr.delegate = self;
+    [_imageView addGestureRecognizer:pgr];    
+}
+- (IBAction)tapGesturePerformed:(id)sender {
+    NSLog(@"%@",sender);
+ //   if (sender.view == UIImageView)
+    {
+    [self performFullScreenAnimation];
+    _isInFullscreen = !_isInFullscreen;
+    }
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void)performFullScreenAnimation
+{
+    [self.widthConstraint pop_removeAllAnimations];
+    [self.heightConstraint pop_removeAllAnimations];
+    
+    POPSpringAnimation *heightAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayoutConstraintConstant];
+    heightAnimation.springBounciness = 10;
+    
+    
+    POPSpringAnimation *animation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayoutConstraintConstant];
+    animation.springBounciness = 10;
+    
+    if (!_isInFullscreen)
+    {
+        animation.toValue = @(600.);
+        heightAnimation.toValue = @(570);
+    }
+    else
+    {
+        animation.toValue = @(126.);
+        heightAnimation.toValue = @(223.);
+    }
+    
+    [self.heightConstraint pop_addAnimation:heightAnimation forKey:@"fullscreen"];
+    [self.widthConstraint pop_addAnimation:animation forKey:@"fullscreen"];
+    
 }
 
 @end
